cb4f21b1266e7e0de7b7341dddbce02ab3587fd7
Braveheart ola <ola20sarumi@hotmail>
2020-06-19 14:24:02 +0300

    more updates from the API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const APP_ID = "36d0d51f";
+  const APP_KEY = "4c00071191fd0bf3d78df8044a6870e1";
+  const exampleReq = useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
